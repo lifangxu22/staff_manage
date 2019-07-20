a4cc5255d806e1a4bb42f2d5f8a2b953ac115d32
lifangxu <lifangxu22@gmail.com>
2019-07-20 13:36:37 +0800

    git test add

---
+++ git_test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	#	 FileName	: git_test.c
+	#	 Author		: lifangxu 
+	#	 Email		: lifangxu22@gmail.com 
+	#	 Created	: Sat 20 Jul 2019 01:35:37 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
